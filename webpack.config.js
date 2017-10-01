8905cd09162dd5b3a9b42d7e5a6afa34aeeaf018
kimberleykelly <kimberley.b.kelly@gmail.com>
2017-10-01 12:36:55 +0100

    I have set up the development enviroment.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractSass = new ExtractTextPlugin({
+    filename: 'style.css'
+})
+module.exports = {
+    entry:'./src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app.js'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.scss/,
+                use: extractSass.extract({ 
+                    use: [
+                        {loader:"css-loader"},
+                        {loader:"sass-loader"},
+                    ]
+                }),
+            }, 
+            {
+                test: /\.js/,
+                exclude: path.resolve(__dirname, './node_modules'),
+                use: [
+                    {loader:"babel-loader"}
+                ]
+            }
+        ]    
+    },
+    plugins:[ 
+        extractSass
+    ]
+}
